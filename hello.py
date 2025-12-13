00af66f78342e1d15f88659eec29e7b8f19da8d4
Hemanth Bathini <hemanthbathini69@gmail.com>
2025-12-13 15:18:17 -0600

    updating practice code

--- hello.py
+++ hello.py
@@ -1,1 +1,63 @@
-print("hello world")
+# name = "hemanth"
+# age = "25"
+# height = 5.10
+# student = True 
+
+# print(f"name: {name}\nheight: {height:.2f}\nage: {str(age)}\nare you a student: {student}")
+
+# print(name.count("h"))
+# print(name.find("a"))
+# print(name.capitalize())
+# print(name.replace("hemanth", "python"))
+# print(name.endswith("qr"))
+# print(type(age))
+
+# a = 10 
+# b = 20
+
+# a,b = b,a
+
+# print(f"a: {a} \nb: {b}")
+
+# a = a + b
+# b = a - b
+# a = a - b
+# print(f"a = {a} \nb = {b}")
+
+# num = "45"
+# num2 = 12.84
+# num3 = 0
+# print(int(num2))
+# print(str(num2))
+# print(type(num2))
+# print(int(num))
+# print(float(num))
+# num2 = True
+# print(num2)
+
+
+# city = input("Enter your city name: ")
+# fav_food = input("Enter you fav food: ")
+# num1 = int(input("enter first num: "))
+# num2 = int(input("enter second num: "))
+# sum = num1 + num2
+# difference = num1 - num2
+# product = num1 * num2
+# average = (num1 + num2)/2
+# print(f"you live in {city} and you love {fav_food}")
+
+# print(f"sum: {sum} \ndifference: {difference} \nproduct: {product} \naverage: {average}")
+
+# num = int(input("enter any number: "))
+
+# if(num%2 == 0):
+#     print("even number")
+# else:
+#     print("odd number")
+
+name = input("enter your name: ")
+
+updated = name.upper()
+updated2 = name.lower()
+print(f"{updated}, \n{updated2}")
+print(name.replace("a", "@"))
--- hello2.py
+++
@@ -1,7 +0,0 @@
-
-#Author : Hemanth Bathini
-#Date : 09/10/24
-#Description : This program prompts the user for their name and greets them using that name
-
-userName = input("please enter your name ")
-print(f"Hello {userName}!")
